fd61ff644c40cbdc920d23dc6e669b3f61cbf603
sneka2405 <sneka24052004@gmail.com>
2025-01-24 17:34:10 +0530

    gh

--- login/src/home.jsx
+++ login/src/home.jsx
@@ -1,32 +1,8 @@
-// import React from 'react'
 
-// export default function home() {
-//   return (
-//     <div>
-//         <div className="header">
-//      <div className="header1">
-     
-//      </div>  
-//       <div className="header2">
-//         <button onClick>HOME</button>
-//         <button onClick>SERVICES</button>
-//         <button onClick>PRODUCTS</button>
-//         <button onClick>ABOUT</button>
-         
-            
-     
-  
-
-//        <a href="..\src\assets\Screenshot 2024-08-02 124954.png"target="_blank">Click<h3></h3></a>
-//     </div>
-//     </div>
-//     </div>
-//   )
-// }
 import React from 'react'
 const Home = () => {
   return (
-    <div>Login Successfully</div>
+    <div>Login Successfullykkkk</div>
   )
 }
 export default Home
